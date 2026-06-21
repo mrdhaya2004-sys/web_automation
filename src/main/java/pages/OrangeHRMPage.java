dbcca52d917585b07c2fbac2648eb9e7972bd3e8
mrdhaya2004-sys <mrdhaya2004@gmail.com>
2026-06-21 20:01:33 +0530

    Add imports for WebDriverWait and Duration

--- src/main/java/pages/OrangeHRMPage.java
+++ src/main/java/pages/OrangeHRMPage.java
@@ -1,6 +1,11 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import wrappers.GenericWrappersWeb;
